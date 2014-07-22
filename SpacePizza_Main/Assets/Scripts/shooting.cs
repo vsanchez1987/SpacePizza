6a5e5f7c9a3958f31d562a1be7d6b87c8a634746
OmarPD <omardel23@comcast.net>
2014-07-21 21:54:09 -0700

    Here is the problem of tthe gun

---
+++ SpacePizza_Main/Assets/Scripts/shooting.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shooting : MonoBehaviour {
+
+
+	public GameObject bullet;
+	private Vector3 direction;
+	public GameObject aim;
+	//public GameObject gun;
+
+
+	// Use this for initialization
+	void Start () {
+
+		bullet = GameObject.Find("Bullet");
+		aim = GameObject.Find("Aim");
+		//gun = GameObject.Find("Gun");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//direction = aim.transform.position - gun.transform.position;
+
+		if(Input.GetMouseButtonDown(0))
+		{
+
+			bullet = Instantiate(Resources.Load("Bullet")) as GameObject;
+			bullet.transform.position = this.transform.position;
+			print(this.transform.position);
+			bullet.rigidbody.AddForce(Vector3.right * 10);
+
+		}
+
+	
+	}
+}
