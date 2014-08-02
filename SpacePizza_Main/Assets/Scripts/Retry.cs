73c2c73c0b948a8b3ff07f76126e9115396c7744
OmarPD <omardel23@comcast.net>
2014-08-01 22:38:58 -0700

    finished the re-spawn system, how it works is simple, if it collides with trigger spawn2, it will spawn there once the health is equal to 0, we can place as many as we want, all we have to do is create them, tag em, and add one line of code on the sript

---
+++ SpacePizza_Main/Assets/Scripts/Retry.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Retry : MonoBehaviour {
+
+
+	private int health =10;
+
+	private Vector3 spawnPoint;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(health <=0)
+		{
+
+			this.transform.position = spawnPoint;
+
+			health =10;
+		}
+
+
+		if(Input.GetKeyDown("r"))
+		{
+			health = 0;
+		}
+
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "spawn1")
+		{
+			spawnPoint = col.gameObject.transform.position;
+		}
+
+		if(col.gameObject.tag == "spawn2")
+		{
+			spawnPoint = col.gameObject.transform.position;
+		}
+
+
+	}
+
+}
